1e3fc44f4b4b26323cfe8c99ddb01a698a417155
Ian L <edx@logendra.com>
2023-02-21 15:24:26 +0000

    basic structure or readme file set up

---
+++ GeneratedREADME.md
@@ -0,0 +1,35 @@
+
+# Project Title 
+test
+
+## Description
+test
+
+## TableOfContents
+tset
+
+## Installation
+test
+
+## Usage
+test
+
+## License
+[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)]
+
+## Contributing 
+test
+
+## Tests
+test
+
+## Questions
+test
+
+## Author
+tset
+
+## Email
+test
+
+
--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ import inquirer from "inquirer";
 import Inq from "inquirer";
 import fs from "fs/promises";
 
-let { title, description, tableOfContents } = await inquirer.prompt([
+let { title, description, tableOfContents, installation, usage, license, contributing, tests, questions, username, email } = await inquirer.prompt([
   {
     type: "input",
     name: "title",
@@ -17,32 +17,129 @@ let { title, description, tableOfContents } = await inquirer.prompt([
   {
     type: "input",
     name: "tableOfContents",
-    message: "Please your Table of Contents",
+    message: "Please enter your Table of Contents",
   },
+
+  {
+    type: "input",
+    name: "installation",
+    message: "Please provide instructions on how to install your project",
+  },
+
+
+  {
+    type: "input",
+    name: "usage",
+    message: "Please enter details of how to use your project",
+  },
+
+  {
+    type: "list",
+    name: "license",
+    message: "What license do you need?",
+    choices: ["Boost", "CCO", "IBM", "MIT"],
+    filter(val) {
+      return val.toLowerCase();
+    },
+  },
+
+
+  {
+    type: "input",
+    name: "contributing",
+    message: "Who contributed to your project",
+  },
+
+  {
+    type: "input",
+    name: "tests",
+    message: "Please enter details of how to use your project",
+  },
+
+  {
+    type: "input",
+    name: "questions",
+    message: "Please end your FAQs here",
+  },
+
+  
+
+
+  {
+    type: "input",
+    name: "username",
+    message: "What is your github username",
+  },
+
+  
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address",
+  },
+
+  
+
+
 
 
 ]);
 
-console.log(response);
-
 let readMeText = `
-#Project Title 
+# Project Title 
 ${title}
 
-##Description
+## Description
 ${description}
 
+## TableOfContents
+${tableOfContents}
+
+## Installation
+${installation}
+
+## Usage
+${usage}
+
+## License
+${generateLicense(license)}
+
+## Contributing 
+${contributing}
+
+## Tests
+${tests}
+
+## Questions
+${questions}
+
+## Author
+${username}
+
+## Email
+${email}
 
 
 `;
 
-fs.writeFile('GeneratedREADME.md',readMeText);
+fs.writeFile("GeneratedREADME.md", readMeText);
 
-// * Description
-//       * Table of Contents
-//       * Installation
-//       * Usage
-//       * License
-//       * Contributing
-//       * Tests
-//       * Questions
+
+function generateLicense(license) {
+
+  if (license === "boost") {
+    return "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)]";
+  }
+
+  if (license === "cco") {
+    return "[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)]";
+  }
+
+  if (license === "ibm") {
+    return "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)]";
+  }
+
+  if (license === "mit") {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  }
+}
